6e78b1b912596294d0ff4a97204ad5b813c4f462
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-03-15 10:54:42 +0700

    Commit Changed

--- app/src/main/java/com/example/myapplication/TestGIT.java
+++ app/src/main/java/com/example/myapplication/TestGIT.java
@@ -3,14 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class Activity3 extends AppCompatActivity {
-private Button button;
+public class TestGIT extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_3);
-        button = findViewById(R.id.button);
+        setContentView(R.layout.activity_test_git);
     }
 }
